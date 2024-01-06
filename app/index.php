985e9d0509b580e04cfc08f799421fb384a32e55
Nikita <nik.qaz321@gmail.com>
2024-01-07 01:28:53 +0300

    Доделана главная страница; Необходимо сделать регистрацию и авторизацию

--- app/index.php
+++ app/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,15 +22,20 @@
 
     <!-- <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> -->
 
-    <title>Наше кино</title>
+    <title>Наше кино - Главная</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/favicon/site.webmanifest">
 
     <!-- Additional CSS Files -->
     <!-- <link rel="stylesheet" href="assets/css/fontawesome.css"> -->
-    <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
@@ -57,7 +71,7 @@
                     <!-- ***** Search End ***** -->
                     <div class="search-input">
                       <form id="search" action="#">
-                        <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
+                        <input type="text" placeholder="Поиск фильма" id='searchText' name="searchKeyword" onkeypress="handle" />
                         <i class="fa fa-search"></i>
                       </form>
                     </div>
@@ -65,11 +79,25 @@
                     <!-- ***** Menu Start ***** -->
                     <ul class="nav">
                         <li><a href="index.html" class="active">Главная</a></li>
-                        <li><a href="browse.html">Фильмы</a></li>
-                        <li><a href="details.html">Details</a></li>
-                        <li><a href="streams.html">Streams</a></li>
-                        <li><a href="profile.html">Профиль<img src="assets/images/profile-header.jpg" alt=""></a></li>
-                    </ul>   
+                        <li><a href="browse.html">Обзор</a></li>
+                        <li><a href="details.html">Детали</a></li>
+                        <li><a href="streams.html">Фильмы</a></li>
+                        <!-- <li><a href="profile.html">Профиль<img src="assets/images/profile-header.jpg" alt=""></a></li> -->
+                        <li id="profileTab" class="profile-menu">
+                          <a href="profile.html">Профиль<img src="assets/images/profile-header.jpg" alt=""></a>
+                          <div id="profileDropdown" class="profile-dropdown">
+                              <a href="login.php">Авторизация</a>
+                              <a href="register.php">Регистрация</a>
+                          </div>
+                      </li>
+                        <!-- <li class="profile-menu">
+                          <a href="profile.html">Профиль</a>
+                          <div class="profile-dropdown">
+                              <a href="login.html">Авторизация</a>
+                              <a href="register.html">Регистрация</a>
+                          </div>
+                      </li> -->
+                    </ul> 
                     <a class='menu-trigger'>
                         <span>Menu</span>
                     </a>
@@ -91,10 +119,10 @@
             <div class="row">
               <div class="col-lg-7">
                 <div class="header-text">
-                  <h6>Welcome To Cyborg</h6>
-                  <h4><em>Browse</em> Our Popular Games Here</h4>
+                  <h6>Добро пожаловать в рекомендательный сервис <br><b><h5>Наше Кино</h5></b></h6>
+                  <h4><em>Начните </em>свое знакомство с русским кино здесь</h4>
                   <div class="main-button">
-                    <a href="browse.html">Browse Now</a>
+                    <a href="browse.html">Вперёд</a>
                   </div>
                 </div>
               </div>
@@ -107,20 +135,62 @@
             <div class="row">
               <div class="col-lg-12">
                 <div class="heading-section">
-                  <h4><em>Most Popular</em> Right Now</h4>
+                  <h4><em>Популярно</em> сейчас</h4>
                 </div>
                 <div class="row">
                   <div class="col-lg-3 col-sm-6">
                     <div class="item">
-                      <img src="assets/images/popular-01.jpg" alt="">
-                      <h4>Fortnite<br><span>Sandbox</span></h4>
+                      <img src="assets/images/popular-film-01.jpg" alt="">
+                      <!-- Нежданный гость - CommonName (Название) Военная мелодрама - Filmino (Сведение о фильме) -->
+                      <h4>Нежданный гость<br><span>Военная мелодрама</span></h4>
                       <ul>
-                        <li><i class="fa fa-star"></i> 4.8</li>
-                        <li><i class="fa fa-download"></i> 2.3M</li>
+                        <!-- 1972 - Year (Год выпуска)-->
+                        <li><i class="fa fa-star"></i> 1972</li>
+                        <!-- 12+ - AgeRestrictions (Возрастные ограничения)-->
+                        <li><i class="fa fa-download"></i>12+</li>
                       </ul>
                     </div>
                   </div>
                   <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <img src="assets/images/popular-film-02.jpg" alt="">
+                      <!-- Нежданный гость - CommonName (Название) Военная мелодрама - Filmino (Сведение о фильме) -->
+                      <h4>Не хочу быть взрослым<br><span>Комедия</span></h4>
+                      <ul>
+                        <!-- 1972 - Year (Год выпуска)-->
+                        <li><i class="fa fa-star"></i> 1982</li>
+                        <!-- 12+ - AgeRestrictions (Возрастные ограничения)-->
+                        <li><i class="fa fa-download"></i>12+</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <img src="assets/images/popular-film-03.jpg" alt="">
+                      <!-- Нежданный гость - CommonName (Название) Военная мелодрама - Filmino (Сведение о фильме) -->
+                      <h4>Падение Кондора<br><span>Драма</span></h4>
+                      <ul>
+                        <!-- 1972 - Year (Год выпуска)-->
+                        <li><i class="fa fa-star"></i>1982</li>
+                        <!-- 12+ - AgeRestrictions (Возрастные ограничения)-->
+                        <li><i class="fa fa-download"></i>16+</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <img src="assets/images/popular-film-04.jpg" alt="">
+                      <!-- Нежданный гость - CommonName (Название) Военная мелодрама - Filmino (Сведение о фильме) -->
+                      <h4>Сержант Фетисов<br><span>Документальный</span></h4>
+                      <ul>
+                        <!-- 1972 - Year (Год выпуска)-->
+                        <li><i class="fa fa-star"></i> 1961</li>
+                        <!-- 12+ - AgeRestrictions (Возрастные ограничения)-->
+                        <li><i class="fa fa-download"></i>12+</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- <div class="col-lg-3 col-sm-6">
                     <div class="item">
                       <img src="assets/images/popular-02.jpg" alt="">
                       <h4>PubG<br><span>Battle S</span></h4>
@@ -149,8 +219,8 @@
                         <li><i class="fa fa-download"></i> 2.3M</li>
                       </ul>
                     </div>
-                  </div>
-                  <div class="col-lg-6">
+                  </div> -->
+                  <!-- <div class="col-lg-6">
                     <div class="item">
                       <div class="row">
                         <div class="col-lg-6 col-sm-6">
@@ -195,10 +265,10 @@
                         <li><i class="fa fa-download"></i> 2.3M</li>
                       </ul>
                     </div>
-                  </div>
+                  </div> -->
                   <div class="col-lg-12">
                     <div class="main-button">
-                      <a href="browse.html">Discover Popular</a>
+                      <a href="browse.html">Подробнее</a>
                     </div>
                   </div>
                 </div>
@@ -211,42 +281,33 @@
           <div class="gaming-library">
             <div class="col-lg-12">
               <div class="heading-section">
-                <h4><em>Your Gaming</em> Library</h4>
+                <h4><em>Ваша</em> фильмотека</h4>
               </div>
               <div class="item">
                 <ul>
                   <li><img src="assets/images/game-01.jpg" alt="" class="templatemo-item"></li>
-                  <li><h4>Dota 2</h4><span>Sandbox</span></li>
-                  <li><h4>Date Added</h4><span>24/08/2036</span></li>
-                  <li><h4>Hours Played</h4><span>634 H 22 Mins</span></li>
-                  <li><h4>Currently</h4><span>Downloaded</span></li>
-                  <li><div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div></li>
-                </ul>
-              </div>
-              <div class="item">
-                <ul>
-                  <li><img src="assets/images/game-02.jpg" alt="" class="templatemo-item"></li>
-                  <li><h4>Fortnite</h4><span>Sandbox</span></li>
-                  <li><h4>Date Added</h4><span>22/06/2036</span></li>
-                  <li><h4>Hours Played</h4><span>740 H 52 Mins</span></li>
-                  <li><h4>Currently</h4><span>Downloaded</span></li>
-                  <li><div class="main-border-button"><a href="#">Donwload</a></div></li>
+                  <li><h4>Нежданный гость</h4><span>Военная мелодрама</span></li>
+                  <li><h4>Дата просмотра</h4><span>05/01/2024</span></li>
+                  <!-- Duration - Длительность (MAIN TABLE) -->
+                  <li><h4>Длительность</h4><span>1:36:50 </span></li>
+                  <li><h4>Статус</h4><span>Просмотрено</span></li>
+                  <li><div class="main-border-button border-no-active"><a href="#">Понравилось</a></div></li>
                 </ul>
               </div>
               <div class="item last-item">
                 <ul>
                   <li><img src="assets/images/game-03.jpg" alt="" class="templatemo-item"></li>
-                  <li><h4>CS-GO</h4><span>Sandbox</span></li>
-                  <li><h4>Date Added</h4><span>21/04/2036</span></li>
-                  <li><h4>Hours Played</h4><span>892 H 14 Mins</span></li>
-                  <li><h4>Currently</h4><span>Downloaded</span></li>
-                  <li><div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div></li>
+                  <li><h4>Не хочу быть взрослым</h4><span>Комедия</span></li>
+                  <li><h4>Дата просмотра</h4><span>12/12/2024</span></li>
+                  <li><h4>Длительность</h4><span>1:18:23</span></li>
+                  <li><h4>Статус</h4><span>Надо просмотреть</span></li>
+                  <li><div class="main-border-button border-active"><a href="#">Просмотрено</a></div></li>
                 </ul>
               </div>
             </div>
             <div class="col-lg-12">
               <div class="main-button">
-                <a href="profile.html">View Your Library</a>
+                <a href="profile.html">К фильмотетке</a>
               </div>
             </div>
           </div>
@@ -260,9 +321,9 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved. 
-          
-          <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a>  Distributed By <a href="https://themewagon.com" target="_blank" >ThemeWagon</a></p>
+          <p>Копирайт © 2024 Наше кино. 
+          <br>Все права зарезирвированы. 
+
         </div>
       </div>
     </div>
@@ -280,7 +341,24 @@
   <script src="assets/js/popup.js"></script>
   <script src="assets/js/custom.js"></script>
 
+  <script>
+    $(document).ready(function () {
+        // Проверьте статус аутентификации пользователя (вам нужно реализовать эту логику)
+        var isAuthenticated = false;  // Замените на вашу фактическую проверку аутентификации
 
+        // Если пользователь не аутентифицирован, покажите выпадающий список при наведении
+        if (!isAuthenticated) {
+            $("#profileTab").hover(
+                function () {
+                    $("#profileDropdown").slideDown('fast');
+                },
+                function () {
+                    $("#profileDropdown").stop().slideUp('fast');
+                }
+            );
+        }
+    });
+</script>
   </body>
 
 </html>
